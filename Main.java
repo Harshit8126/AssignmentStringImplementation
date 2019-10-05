8a2369a6a7200e299ff3668521cba70952dd395b
HARSHIT VERMA <46756130+Harshit8126@users.noreply.github.com>
2019-10-05 22:24:31 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter the String");
+        System.out.println("Enter the String:");
         String a = sc.next();
-        System.out.println("enter the substrng index: ");
+        System.out.println("Enter the substrng index: ");
         int b = sc.nextInt();
         String c = a.substring(b);
         System.out.println(c);
